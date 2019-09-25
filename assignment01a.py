4ec41547a417fb8b27703433da90eb2d39bc9eb0
haltseva <54915119+haltseva@users.noreply.github.com>
2019-09-25 13:24:31 +0300

    Update assignment01a.py

--- assignment01a.py
+++ assignment01a.py
@@ -89,10 +89,37 @@ array1=['This is ','That kept ','That waked ','That married ','That kissed ','Th
 array2=['the farmer sowing his corn,','the cock that crow\'d in the morn,','the priest all shaven and shorn,',
 'the man all tatter\'d and torn,','the maiden all forlorn,','the cow with the crumpled horn,','the dog, ','the cat, ','the rat,',
 'the malt ','the house that Jack built.']
-i=0
-while i< len(array1):
-    print(array1[i]+array2[len(array2)-i]+'\n')
-    print('\n')
-    i+=1
+array3=['the house that Jack built.','the malt ','the rat,','the cat, ','the dog, ','the cow with the crumpled horn,','the maiden all forlorn,',
+'the man all tatter\'d and torn,','the priest all shaven and shorn,','the cock that crow\'d in the morn,','the farmer sowing his corn,']
+a='This is '
+b='That kept '
+c='That waked '
+d='That married '
+e='That kissed '
+f='That milk\'d '
+g='That tossed '
+h='That worried '
+i='That killed '
+k='That ate '
+l='That lay in '
+
+b1='the cock that crow\'d in the morn,'
+c1='the priest all shaven and shorn,'
+d1='the man all tatter\'d and torn,'
+e1='the maiden all forlorn,'
+f1='the cow with the crumpled horn,'
+g1='the dog, '
+h1='the cat, '
+i1='the rat,'
+k1='the malt '
+l1='the house that Jack built.'
+print(f"{a}{l1}\n\n{a}{k1}\n{l}{l1}\n\n{a}{i1}\n{k}{k1}\n{l}{l1}\n\n{a}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n"
+f"{a}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n{a}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n"
+f"{a}{e1}\n{f}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n{a}{d1}\n{e}{e1}\n{f}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n"
+f"{a}{c1}\n{d}{d1}\n{f}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n{a}{b1}\n{c}{c1}\n{d}{d1}\n{e}{e1}\n{f}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}\n\n"
+f"{a}{a1}\n{b}{b1}\n{c}{c1}\n{d}{d1}\n{f}{f1}\n{g}{g1}\n{h}{h1}\n{i}{i1}\n{k}{k1}\n{l}{l1}")
+
+
+
 
 
